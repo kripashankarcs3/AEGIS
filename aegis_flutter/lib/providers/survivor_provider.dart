e50a7cf30e5cdbc4687e5fcc71f8b10605a0b641
bhoomisha <bhoomisharma3104@gmail.com>
2026-07-15 23:11:27 +0530

    implement chat and survivor providers

--- aegis_flutter/lib/providers/survivor_provider.dart
+++ aegis_flutter/lib/providers/survivor_provider.dart
@@ -1,2 +1,37 @@
-// Survivor Provider
-// Survivor map state for radar
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/survivor_node.dart';
+import '../core/status_beacon.dart';
+import '../services/storage_service.dart';
+import 'chat_provider.dart' show myIdentityProvider, sendPacketProvider;
+
+final statusBeaconProvider = Provider<StatusBeacon>((ref) {
+  final beacon = StatusBeacon();
+  beacon.myIdentity = ref.watch(myIdentityProvider);
+  beacon.broadcastToMesh = (packet) => ref.watch(sendPacketProvider)('broadcast', packet);
+  beacon.start();
+  ref.onDispose(beacon.stop);
+  return beacon;
+});
+
+class SurvivorMapNotifier extends StateNotifier<Map<String, SurvivorNode>> {
+  SurvivorMapNotifier() : super({}) {
+    _load();
+  }
+
+  Future<void> _load() async {
+    final nodes = await StorageService.getAllSurvivorNodes();
+    state = {for (final n in nodes) n.id: n};
+  }
+
+  // called from mesh_router.dart's incoming handler once wired up
+  void updateFromIncoming(SurvivorNode node) {
+    state = {...state, node.id: node};
+    StorageService.saveSurvivorNode(node);
+  }
+
+  void refresh() => _load();
+}
+
+final survivorProvider = StateNotifierProvider<SurvivorMapNotifier, Map<String, SurvivorNode>>(
+  (ref) => SurvivorMapNotifier(),
+);
